14db54022c57f6ca5ef49307188f3e30a246a9c6
jhyun94 <hyunjonathan@gmail.com>
2017-04-10 17:05:27 -0500

    able to filter by type of media

--- public/main.js
+++ public/main.js
@@ -13,11 +13,48 @@ $(document).ready(function(){
     return new Date(b.item_created) - new Date(a.item_created);
   })
   appendInitial(json);
-
   $(".btn").on("click", function(){
     loadMore(json);
     buttonChecker();
   })
+
+  $(".filter li").on("click", function(){
+    var filtered = filter(this.innerText);
+    $(".row").empty();
+    for (var i =0; i < filtered.items.length; i++){
+      $(".row").append(product(filtered.items[i]));
+    }
+  })
+
+  function filter(category){
+    var filtered = Object.assign({}, json);
+    switch (category) {
+      case "Manual":
+        filtered.items = filtered.items.filter(function(item){
+          if (item.service_name == "Manual"){
+            return item;
+          }
+        })
+        return filtered;
+        break;
+      case "Twitter":
+        filtered.items = filtered.items.filter(function(item){
+          if (item.service_name == "Twitter"){
+            return item;
+          }
+        })
+        return filtered;
+        break;
+      case "Instagram":
+        filtered.items = filtered.items.filter(function(item){
+          if (item.service_name == "Instagram"){
+            return item;
+          }
+        })
+        return filtered;
+        break;  
+    }
+  }
 
   function loadMore(data){
     for (var i =0; i < 4; i++){
